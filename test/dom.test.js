1b059d4f75b4220d50845931b00ea1368f6d1c4c
Steve Brownlee <chortlehoort@gmail.com>
2020-04-18 23:37:54 -0500

    Testing DOM now

--- test/dom.test.js
+++ test/dom.test.js
@@ -1,12 +1,53 @@
 import { makePottery } from "../src/scripts/PotteryWheel.js"
 import { firePottery } from "../src/scripts/Kiln.js"
 import { usePottery, toSellOrNotToSell } from "../src/scripts/PotteryCatalog.js"
+import { PotteryList } from "../src/scripts/PotteryList.js"
+const puppeteer = require("puppeteer")
 
-let piece = null
+let mug = null
+let ornament = null
+let platter = null
+
+const $ = require('jquery');
 
 beforeAll(() => {
-    piece = makePottery("Mug", 10, 8)
+    mug = toSellOrNotToSell(firePottery(makePottery("Mug", 10, 8), 3000))
+    ornament = toSellOrNotToSell(firePottery(makePottery("Ornament", 2, 5), 2000))
+    platter = toSellOrNotToSell(firePottery(makePottery("Platter", 7, 2), 2000))
 })
+
+test('Pottery is rendered to DOM', async () => {
+    let browser = await puppeteer.launch({
+        headless: true
+    })
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8080');
+    await page.waitForSelector('.potteryList')
+
+    const html = await page.$eval('.potteryList', _ => _.innerHTML)
+
+    expect(html).toBe(`
+<section class="pottery">
+    <h2 class="pottery__shape">Ornament</h2>
+    <div class="pottery_properties">
+        Item weighs 2 grams and is 5 cm in height
+    </div>
+    <div class="pottery__price">
+        Price is $20
+    </div>
+</section>
+<section class="pottery">
+    <h2 class="pottery__shape">Platter</h2>
+    <div class="pottery_properties">
+        Item weighs 7 grams and is 2 cm in height
+    </div>
+    <div class="pottery__price">
+        Price is $40
+    </div>
+</section>`);
+})
+
+
 
 
 
